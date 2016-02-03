1d19cab1d5ac5f751ea57c921beda7854a8d938b
Nicola Gruhl <ngruhl@welfenlab.de>
2016-02-03 11:41:43 +0100

    (feature) Added port names

--- src/pattern-matching.js
+++ src/pattern-matching.js
@@ -1,8 +1,14 @@
+var compLib = require('@buggyorg/component-library').getComponentLibrary()
+
+function getInputs (meta) {
+  return Object.keys(compLib[meta]['inputPorts'])
+}
+
+function getOutputs (meta) {
+  return Object.keys(compLib[meta]['outputPorts'])
+}
+
 export function convert (expression) {
-  var fs = require('fs')
-  var grlib = require('graphlib')
-  // var componentLib = require('@buggyorg/component-library')
-  // var compLib = componentLib.getComponentLibrary()
   var graph = expression
   var nodeId = 0
   var rules = expression.node('PARENT')['rules']
@@ -34,7 +40,6 @@ export function convert (expression) {
   ruleTriple.sort(function (a, b) {
     return b.numberOfConstants - a.numberOfConstants
   })
-  console.log('Before:\n' + ruleTriple)
   var maxRuleCount = ruleTriple[0]['numberOfConstants']
   var currentStep = maxRuleCount
   var orInputs = []
@@ -57,15 +62,15 @@ export function convert (expression) {
       nodeId++
       graph.setNode(nodeNot, {nodeType: 'process', meta: 'logic/not', type: 'atomic', parent: 'PARENT'})
       graph.setParent(nodeNot, 'PARENT')
-      graph.setEdge(lastOr, nodeNot)
+      graph.setEdge(lastOr, nodeNot, {'outPort': getOutputs('logic/or')[0], 'inPort': getInputs('logic/not')[0]})
       if (ruleTriple[r]['numberOfConstants'] > 0) {
         // create the and node
         var nodeAnd = nodeId + '_AND'
         nodeId++
         graph.setNode(nodeAnd, {nodeType: 'process', meta: 'logic/and', type: 'atomic', parent: 'PARENT'})
         graph.setParent(nodeAnd, 'PARENT')
-        graph.setEdge(nodeNot, nodeAnd)
-        graph.setEdge(ruleTriple[r]['endBoolNode'], nodeAnd)
+        graph.setEdge(nodeNot, nodeAnd, {'outPort': getOutputs('logic/not')[0], 'inPort': getInputs('logic/and')[0]})
+        graph.setEdge(ruleTriple[r]['endBoolNode'], nodeAnd, {'outPort': getOutputs(graph.node(ruleTriple[r]['endBoolNode'])['meta'])[0], 'inPort': getInputs('logic/and')[1]})
         ruleTriple[r]['endBoolNode'] = nodeAnd
       } else {
         ruleTriple[r]['endBoolNode'] = nodeNot
@@ -74,7 +79,6 @@ export function convert (expression) {
       orInputs.push(ruleTriple[r]['endBoolNode'])
     }
   }
-  console.log('After:\n' + ruleTriple)
 
   for (var ru = 0; ru < ruleTriple.length; ru++) {
     var outports = ruleTriple[ru]['rule']['outputs']
@@ -88,20 +92,19 @@ export function convert (expression) {
         nodeId++
         graph.setNode(nodeConst, {nodeType: 'process', meta: 'logic/const', type: 'atomic', parent: 'PARENT'})
         graph.setParent(nodeConst, 'PARENT')
-        graph.setEdge(nodeConst, nodeDemux)
+        graph.setEdge(nodeConst, nodeDemux, {'outPort': getOutputs('logic/const')[0], 'inPort': getInputs('logic/demux')[0]})
       } else {
         var inports = ruleTriple[ru]['rule']['inputs']
         for (var i = 0; i < inports.length; i++) {
           if (inports[i]['value'] === outports[outport]['value']) {
-            graph.setEdge('PARENT', nodeDemux, inports[i]['name'])
+            graph.setEdge('PARENT', nodeDemux, {'outPort': inports[i]['name'], 'inPort': getInputs('logic/demux')[0]})
           }
         }
       }
-      graph.setEdge(ruleTriple[ru]['endBoolNode'], nodeDemux)
-      graph.setEdge(nodeDemux, 'PARENT', outports[outport]['name'])
+      graph.setEdge(ruleTriple[ru]['endBoolNode'], nodeDemux, {'outPort': getOutputs(graph.node(ruleTriple[ru]['endBoolNode'])['meta'])[0], 'inPort': getInputs('logic/demux')[1]})
+      graph.setEdge(nodeDemux, 'PARENT', {'outPort': getOutputs('logic/demux')[0], 'inPort': outports[outport]['name']})
     }
   }
-  fs.writeFileSync('test/fixtures/testgraphBig.graphlib', JSON.stringify(grlib.json.write(graph), null, 2))
   return graph
 }
 
@@ -110,12 +113,12 @@ function createEqualAndConst (constValue, edgeName, nodeId, graph) {
   nodeId++
   graph.setNode(nodeEqual, {nodeType: 'process', meta: 'logic/equal', type: 'atomic', parent: 'PARENT'})
   graph.setParent(nodeEqual, 'PARENT')
-  graph.setEdge('PARENT', nodeEqual, edgeName)
+  graph.setEdge('PARENT', nodeEqual, {'outPort': edgeName, 'inPort': getInputs('logic/equal')[0]})
   var nodeConst = nodeId + '_CONST'
   nodeId++
   graph.setNode(nodeConst, {nodeType: 'process', meta: 'logic/const', type: 'atomic', parent: 'PARENT'})
   graph.setParent(nodeConst, 'PARENT')
-  graph.setEdge(nodeConst, nodeEqual)
+  graph.setEdge(nodeConst, nodeEqual, {'outPort': getOutputs('logic/const')[0], 'inPort': getInputs('logic/equal')[1]})
   return {equalNode: nodeEqual, newNodeId: nodeId}
 }
 
@@ -127,15 +130,15 @@ function createAtomics (inputs, nodeId, graph, nodeName) {
   nodeId++
   graph.setNode(node, {nodeType: 'process', meta: 'logic/' + nodeName.toLowerCase(), type: 'atomic', parent: 'PARENT'})
   graph.setParent(node, 'PARENT')
-  graph.setEdge(inputs[0], node)
-  graph.setEdge(inputs[1], node)
+  graph.setEdge(inputs[0], node, {'outPort': getOutputs(graph.node(inputs[0])['meta'])[0], 'inPort': getInputs('logic/' + nodeName.toLowerCase())[0]})
+  graph.setEdge(inputs[1], node, {'outPort': getOutputs(graph.node(inputs[1])['meta'])[0], 'inPort': getInputs('logic/' + nodeName.toLowerCase())[1]})
   for (var i = 2; i < inputs.length; i++) {
     var newNode = nodeId + '_' + nodeName
     nodeId++
     graph.setNode(newNode, {nodeType: 'process', meta: 'logic/' + nodeName.toLowerCase(), type: 'atomic', parent: 'PARENT'})
     graph.setParent(newNode, 'PARENT')
-    graph.setEdge(inputs[i], newNode)
-    graph.setEdge(node, newNode)
+    graph.setEdge(inputs[i], newNode, {'outPort': getOutputs(graph.node(inputs[i])['meta'])[0], 'inPort': getInputs('logic/' + nodeName.toLowerCase())[0]})
+    graph.setEdge(node, newNode, {'outPort': getOutputs('logic/' + nodeName.toLowerCase())[0], 'inPort': getInputs('logic/' + nodeName.toLowerCase())[1]})
     node = newNode
   }
   return {nodeId: nodeId, node: node}
--- test/pattern-matching.js
+++ test/pattern-matching.js
@@ -5,14 +5,20 @@ var patternMatching = require('../src/pattern-matching.js')
 var grlib = require('graphlib')
 var fs = require('fs')
 
-var expression = grlib.json.read(JSON.parse(fs.readFileSync('test/fixtures/testexpressionBig.graphlib')))
+var expressionBig = grlib.json.read(JSON.parse(fs.readFileSync('test/fixtures/testexpressionBig.graphlib')))
+var expression = grlib.json.read(JSON.parse(fs.readFileSync('test/fixtures/testexpression.graphlib')))
 describe('', function () {
   it('', function () {
     var d = patternMatching.convert(expression)
-    fs.writeFileSync('testgraph.graphlib', JSON.stringify(grlib.json.write(d), null, 2))
+    var dBig = patternMatching.convert(expressionBig)
+    // fs.writeFileSync('test/fixtures/testgraph.graphlib', JSON.stringify(grlib.json.write(d), null, 2))
+    // fs.writeFileSync('test/fixtures/testgraphBig.graphlib', JSON.stringify(grlib.json.write(dBig), null, 2))
     var curGraph = grlib.json.write(d)
+    var curGraphBig = grlib.json.write(dBig)
     var cmpGraph = JSON.parse(fs.readFileSync('test/fixtures/testgraph.graphlib'))
+    var cmpGraphBig = JSON.parse(fs.readFileSync('test/fixtures/testgraphBig.graphlib'))
 
     expect(curGraph).to.deep.equal(cmpGraph)
+    expect(curGraphBig).to.deep.equal(cmpGraphBig)
   })
 })
