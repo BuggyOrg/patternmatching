d62ec75d5a612fb30eac4c581cb0d246ac59436a
Nicola Gruhl <ngruhl@welfenlab.de>
2016-05-25 15:31:21 +0200

    (Fix)

--- src/pattern-matching.js
+++ src/pattern-matching.js
@@ -1,7 +1,6 @@
 import libConnection from '@buggyorg/component-library'
 import _ from 'lodash'
 var compLib = libConnection('http://quasar:9200')
-var grlib = require('graphlib')
 
 function getInputs (nodesObj, meta) {
   return Object.keys(nodesObj[meta]['inputPorts'])
@@ -11,12 +10,23 @@ function getOutputs (nodesObj, meta) {
   return Object.keys(nodesObj[meta]['outputPorts'])
 }
 
+function getMeta (nodes, node) {
+  for (var i = 0; i < nodes.length; i++) {
+    if (nodes[i].name === node) {
+      return nodes[i].meta
+    }
+  }
+  return
+}
+
 var nodesIds = [
   {id: 'logic/equal', version: '0.1.0'},
   {id: 'logic/and', version: '0.1.0'},
   {id: 'logic/or', version: '0.1.0'},
   {id: 'logic/not', version: '0.1.0'},
-  {id: 'logic/demux', version: '0.1.0'}
+  {id: 'logic/demux', version: '0.1.0'},
+  {id: 'math/const', version: '0.1.0'},
+  {id: 'control/consume', version: '0.1.0'}
 ]
 
 export function convert (expression) {
@@ -29,8 +39,9 @@ export function convert (expression) {
 
     var graph = expression
     var matchNode = graph.nodes()[0]
-    graph.setNode(matchNode, {'rules': graph.node(matchNode).rules, 'nodeType': 'process', 'atomic': false, 'inputPorts': {}, 'outputPorts': {}, 'implementation': {}})
-    var innerGraph = new grlib.Graph({directed: true, compound: true, multigraph: true})
+    graph.setNode(matchNode, {'id': matchNode, 'rules': graph.node(matchNode).rules, 'nodeType': 'process', 'atomic': false, 'inputPorts': {}, 'outputPorts': {}, 'implementation': {}})
+    var innerGraph = {'nodes': [], 'edges': []}
+    // var innerGraph = new grlib.Graph({directed: true, compound: true, multigraph: true})
     var nodeId = 0
     var allRules = []
     for (var exp = 0; exp < expression.nodes().length; exp++) {
@@ -54,7 +65,7 @@ export function convert (expression) {
         }
       }
       if (equalNodes.length > 1) {
-        res = createAtomics(equalNodes, nodeId, innerGraph, 'AND', nodesObj, matchNode)
+        res = createAtomics(equalNodes, nodeId, innerGraph, 'AND', nodesObj)
         nodeId = res['nodeId']
         ruleTriple.push({rule: rules[rule], numberOfConstants: numberOfConstants, endBoolNode: res['node']})
       } else if (equalNodes.length === 1) {
@@ -78,7 +89,7 @@ export function convert (expression) {
       } else {
         if (ruleTriple[r]['numberOfConstants'] !== currentStep) {
           currentStep = ruleTriple[r]['numberOfConstants']
-          res = createAtomics(orInputs, nodeId, innerGraph, 'OR', nodesObj, matchNode)
+          res = createAtomics(orInputs, nodeId, innerGraph, 'OR', nodesObj)
           nodeId = res['nodeId']
           lastOr = res['node']
           orInputs = [lastOr]
@@ -86,18 +97,23 @@ export function convert (expression) {
         // create the not node
         var nodeNot = nodeId + '_NOT'
         nodeId++
-        innerGraph.setNode(nodeNot, {nodeType: 'process', meta: 'logic/not', type: 'atomic'})
-        innerGraph.setParent(nodeNot, matchNode)
+        innerGraph.nodes.push({'name': nodeNot, 'meta': 'logic/not'})
+        // innerGraph.setNode(nodeNot, {nodeType: 'process', meta: 'logic/not', type: 'atomic'})
+        // innerGraph.setParent(nodeNot, matchNode)
 
-        innerGraph.setEdge(lastOr, nodeNot, {'outPort': getOutputs(nodesObj, 'logic/or')[0], 'inPort': getInputs(nodesObj, 'logic/not')[0]})
+        innerGraph.edges.push({'from': lastOr + ':' + getOutputs(nodesObj, 'logic/or')[0], 'to': nodeNot + ':' + getInputs(nodesObj, 'logic/not')[0]})
+        // innerGraph.setEdge(lastOr, nodeNot, {'outPort': getOutputs(nodesObj, 'logic/or')[0], 'inPort': getInputs(nodesObj, 'logic/not')[0]})
         if (ruleTriple[r]['numberOfConstants'] > 0) {
           // create the and node
           var nodeAnd = nodeId + '_AND'
           nodeId++
-          innerGraph.setNode(nodeAnd, {nodeType: 'process', meta: 'logic/and', type: 'atomic'})
-          innerGraph.setParent(nodeAnd, matchNode)
-          innerGraph.setEdge(nodeNot, nodeAnd, {'outPort': getOutputs(nodesObj, 'logic/not')[0], 'inPort': getInputs(nodesObj, 'logic/and')[0]})
-          innerGraph.setEdge(ruleTriple[r]['endBoolNode'], nodeAnd, {'outPort': getOutputs(nodesObj, innerGraph.node(ruleTriple[r]['endBoolNode'])['meta'])[0], 'inPort': getInputs(nodesObj, 'logic/and')[1]})
+          innerGraph.nodes.push({'name': nodeAnd, 'meta': 'logic/and'})
+          // innerGraph.setNode(nodeAnd, {nodeType: 'process', meta: 'logic/and', type: 'atomic'})
+          // innerGraph.setParent(nodeAnd, matchNode)
+          innerGraph.edges.push({'from': nodeNot + ':' + getOutputs(nodesObj, 'logic/not')[0], 'to': nodeAnd + ':' + getInputs(nodesObj, 'logic/and')[0]})
+          // innerGraph.setEdge(nodeNot, nodeAnd, {'outPort': getOutputs(nodesObj, 'logic/not')[0], 'inPort': getInputs(nodesObj, 'logic/and')[0]})
+          innerGraph.edges.push({'from': ruleTriple[r]['endBoolNode'] + ':' + getOutputs(nodesObj, getMeta(innerGraph.nodes, ruleTriple[r]['endBoolNode']))[0], 'to': nodeAnd + ':' + getInputs(nodesObj, 'logic/and')[1]})
+          // innerGraph.setEdge(ruleTriple[r]['endBoolNode'], nodeAnd, {'outPort': getOutputs(nodesObj, innerGraph.node(ruleTriple[r]['endBoolNode'])['meta'])[0], 'inPort': getInputs(nodesObj, 'logic/and')[1]})
           ruleTriple[r]['endBoolNode'] = nodeAnd
         } else {
           ruleTriple[r]['endBoolNode'] = nodeNot
@@ -113,24 +129,37 @@ export function convert (expression) {
         var nodeDemux = nodeId + '_DEMUX'
         nodeId++
         if (!outports[outport]['variable']) {
-          innerGraph.setNode(nodeDemux, {nodeType: 'process', meta: 'logic/demux', type: 'atomic', values: [{'value': outports[outport]['value/const'], 'port': getInputs(nodesObj, 'logic/demux')[0]}]})
+          innerGraph.nodes.push({'name': nodeDemux, 'meta': 'logic/demux'})
+          var nodeConst = nodeId + '_CONST'
+          nodeId++
+          innerGraph.nodes.push({'name': nodeConst, 'meta': 'math/const', 'params': {'value': outports[outport]['value/const']}})
+          innerGraph.edges.push({'from': nodeConst + ':' + getOutputs(nodesObj, 'math/const')[0], 'to': nodeDemux + ':' + getInputs(nodesObj, 'logic/demux')[0]})
+          // innerGraph.setNode(nodeDemux, {nodeType: 'process', meta: 'logic/demux', type: 'atomic', values: [{'value': outports[outport]['value/const'], 'port': getInputs(nodesObj, 'logic/demux')[0]}]})
         } else {
-          innerGraph.setNode(nodeDemux, {nodeType: 'process', meta: 'logic/demux', type: 'atomic'})
+          innerGraph.nodes.push({'name': nodeDemux, 'meta': 'logic/demux'})
+          // innerGraph.setNode(nodeDemux, {nodeType: 'process', meta: 'logic/demux', type: 'atomic'})
           var inports = ruleTriple[ru]['rule']['inputs']
           for (var i = 0; i < inports.length; i++) {
             if (inports[i]['value'] === outports[outport]['value']) {
-              innerGraph.setEdge(inports[i]['name'], nodeDemux, {'inPort': getInputs(nodesObj, 'logic/demux')[0]})
+              innerGraph.edges.push({'from': inports[i]['name'], 'to': nodeDemux + ':' + getInputs(nodesObj, 'logic/demux')[0]})
+              // innerGraph.setEdge(inports[i]['name'], nodeDemux, {'inPort': getInputs(nodesObj, 'logic/demux')[0]})
               graph.node(matchNode).inputPorts[inports[i]['name']] = inports[i]['type']
             }
           }
         }
-        innerGraph.setParent(nodeDemux, matchNode)
-        innerGraph.setEdge(ruleTriple[ru]['endBoolNode'], nodeDemux, {'outPort': getOutputs(nodesObj, innerGraph.node(ruleTriple[ru]['endBoolNode'])['meta'])[0], 'inPort': getInputs(nodesObj, 'logic/demux')[1]})
-        innerGraph.setEdge(nodeDemux, outports[outport]['name'], {'outPort': getOutputs(nodesObj, 'logic/demux')[0]})
+        // innerGraph.setParent(nodeDemux, matchNode)
+        innerGraph.edges.push({'from': ruleTriple[ru]['endBoolNode'] + ':' + getOutputs(nodesObj, getMeta(innerGraph.nodes, ruleTriple[ru]['endBoolNode']))[0], 'to': nodeDemux + ':' + getInputs(nodesObj, 'logic/demux')[1]})
+        // innerGraph.setEdge(ruleTriple[ru]['endBoolNode'], nodeDemux, {'outPort': getOutputs(nodesObj, innerGraph.node(ruleTriple[ru]['endBoolNode'])['meta'])[0], 'inPort': getInputs(nodesObj, 'logic/demux')[1]})
+        innerGraph.edges.push({'from': nodeDemux + ':' + getOutputs(nodesObj, 'logic/demux')[0], 'to': outports[outport]['name']})
+        var nodeConsume = nodeId + '_CONSUME'
+        nodeId++
+        innerGraph.nodes.push({'name': nodeConsume, 'meta': 'control/consume'})
+        innerGraph.edges.push({'from': nodeDemux + ':' + getOutputs(nodesObj, 'logic/demux')[1], 'to': nodeConsume + ':' + getInputs(nodesObj, 'control/consume')[0]})
+        // innerGraph.setEdge(nodeDemux, outports[outport]['name'], {'outPort': getOutputs(nodesObj, 'logic/demux')[0]})
         graph.node(matchNode).outputPorts[outports[outport]['name']] = outports[outport]['type']
       }
     }
-    graph.node(matchNode).implementation = grlib.json.write(innerGraph)
+    graph.node(matchNode).implementation = innerGraph
     return graph
   }).catch(function (err) { console.error(err) })
 }
@@ -138,30 +167,42 @@ export function convert (expression) {
 function createEqualAndConst (constValue, edgeName, edgeType, nodeId, innerGraph, nodesObj, matchNode, graph) {
   var nodeEqual = nodeId + '_EQUAL'
   nodeId++
-  innerGraph.setNode(nodeEqual, {nodeType: 'process', meta: 'logic/equal', type: 'atomic', values: [{'value': constValue, 'port': getInputs(nodesObj, 'logic/equal')[1]}]})
-  innerGraph.setParent(nodeEqual, matchNode)
-  innerGraph.setEdge(edgeName, nodeEqual, {'inPort': getInputs(nodesObj, 'logic/equal')[0]})
+  innerGraph.nodes.push({'name': nodeEqual, 'meta': 'logic/equal'})
+  // innerGraph.setNode(nodeEqual, {nodeType: 'process', meta: 'logic/equal', type: 'atomic', values: [{'value': constValue, 'port': getInputs(nodesObj, 'logic/equal')[1]}]})
+  // innerGraph.setParent(nodeEqual, matchNode)
+  innerGraph.edges.push({'from': edgeName, 'to': nodeEqual + ':' + getInputs(nodesObj, 'logic/equal')[0]})
+  // innerGraph.setEdge(edgeName, nodeEqual, {'inPort': getInputs(nodesObj, 'logic/equal')[0]})
+  var nodeConst = nodeId + '_CONST'
+  nodeId++
+  innerGraph.nodes.push({'name': nodeConst, 'meta': 'math/const', 'params': {'value': constValue}})
+  innerGraph.edges.push({'from': nodeConst + ':' + getOutputs(nodesObj, 'math/const')[0], 'to': nodeEqual + ':' + getInputs(nodesObj, 'logic/equal')[1]})
   graph.node(matchNode).inputPorts[edgeName] = edgeType
   return {equalNode: nodeEqual, newNodeId: nodeId}
 }
 
-function createAtomics (inputs, nodeId, graph, nodeName, nodesObj, matchNode) {
+function createAtomics (inputs, nodeId, innerGraph, nodeName, nodesObj) {
   if (inputs.length === 1) {
     return {nodeId: nodeId, node: inputs[0]}
   }
   var node = nodeId + '_' + nodeName
   nodeId++
-  graph.setNode(node, {nodeType: 'process', meta: 'logic/' + nodeName.toLowerCase(), type: 'atomic'})
-  graph.setParent(node, matchNode)
-  graph.setEdge(inputs[0], node, {'outPort': getOutputs(nodesObj, graph.node(inputs[0])['meta'])[0], 'inPort': getInputs(nodesObj, 'logic/' + nodeName.toLowerCase())[0]})
-  graph.setEdge(inputs[1], node, {'outPort': getOutputs(nodesObj, graph.node(inputs[1])['meta'])[0], 'inPort': getInputs(nodesObj, 'logic/' + nodeName.toLowerCase())[1]})
+  innerGraph.nodes.push({'name': node, 'meta': 'logic/' + nodeName.toLowerCase()})
+  // innerGraph.setNode(node, {nodeType: 'process', meta: 'logic/' + nodeName.toLowerCase(), type: 'atomic'})
+  // innerGraph.setParent(node, matchNode)
+  innerGraph.edges.push({'from': inputs[0] + ':' + getOutputs(nodesObj, getMeta(innerGraph.nodes, inputs[0]))[0], 'to': node + ':' + getInputs(nodesObj, 'logic/' + nodeName.toLowerCase())[0]})
+  // graph.setEdge(inputs[0], node, {'outPort': getOutputs(nodesObj, graph.node(inputs[0])['meta'])[0], 'inPort': getInputs(nodesObj, 'logic/' + nodeName.toLowerCase())[0]})
+  innerGraph.edges.push({'from': inputs[1] + ': ' + getOutputs(nodesObj, getMeta(innerGraph.nodes, inputs[1]))[0], 'to': node + ':' + getInputs(nodesObj, 'logic/' + nodeName.toLowerCase())[1]})
+  // graph.setEdge(inputs[1], node, {'outPort': getOutputs(nodesObj, graph.node(inputs[1])['meta'])[0], 'inPort': getInputs(nodesObj, 'logic/' + nodeName.toLowerCase())[1]})
   for (var i = 2; i < inputs.length; i++) {
     var newNode = nodeId + '_' + nodeName
     nodeId++
-    graph.setNode(newNode, {nodeType: 'process', meta: 'logic/' + nodeName.toLowerCase(), type: 'atomic'})
-    graph.setParent(newNode, matchNode)
-    graph.setEdge(inputs[i], newNode, {'outPort': getOutputs(nodesObj, graph.node(inputs[i])['meta'])[0], 'inPort': getInputs(nodesObj, 'logic/' + nodeName.toLowerCase())[0]})
-    graph.setEdge(node, newNode, {'outPort': getOutputs(nodesObj, 'logic/' + nodeName.toLowerCase())[0], 'inPort': getInputs(nodesObj, 'logic/' + nodeName.toLowerCase())[1]})
+    innerGraph.nodes.push({'name': newNode, 'meta': 'logic/' + nodeName.toLowerCase()})
+    // graph.setNode(newNode, {nodeType: 'process', meta: 'logic/' + nodeName.toLowerCase(), type: 'atomic'})
+    // graph.setParent(newNode, matchNode)
+    innerGraph.edges.push({'from': inputs[i] + ':' + getOutputs(nodesObj, getMeta(innerGraph.nodes, inputs[i]))[0], 'to': newNode + getInputs(nodesObj, 'logic/' + nodeName.toLowerCase())[0]})
+    // graph.setEdge(inputs[i], newNode, {'outPort': getOutputs(nodesObj, graph.node(inputs[i])['meta'])[0], 'inPort': getInputs(nodesObj, 'logic/' + nodeName.toLowerCase())[0]})
+    innerGraph.edges.push({'from': node + ':' + getOutputs(nodesObj, 'logic/' + nodeName.toLowerCase())[0], 'to': newNode + ':' + getInputs(nodesObj, 'logic/' + nodeName.toLowerCase())[1]})
+    // graph.setEdge(node, newNode, {'outPort': getOutputs(nodesObj, 'logic/' + nodeName.toLowerCase())[0], 'inPort': getInputs(nodesObj, 'logic/' + nodeName.toLowerCase())[1]})
     node = newNode
   }
   return {nodeId: nodeId, node: node}
